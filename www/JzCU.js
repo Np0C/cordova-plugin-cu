d6d15ae2a296a8b25e49d9b895061bd8bcb60213
Sam Tsai <sam.iagd@hotmail.com>
2015-09-08 02:35:14 +0800

    change JzCU.js

--- www/JzCU.js
+++ www/JzCU.js
@@ -1,31 +1,39 @@
+//
+//exports.checkUpdate = function (args, onfulfill, onreject) {
+//    var ThenFail = window.ThenFail;
+//    var promise;
+//
+//    if (ThenFail && !onfulfill && !onreject) {
+//        promise = new ThenFail();
+//    }
+//    if(!args) {
+//        args = {};
+//    }
+//
+//    cordova
+//        .exec(function () {
+//            if (promise) {
+//                promise.resolve();
+//            } else if (onfulfill) {
+//                onfulfill();
+//            }
+//        }, function (err) {
+//            if (promise) {
+//                promise.reject(err);
+//            } else if (onreject) {
+//                onreject(err);
+//            }
+//        }, 'JzCU', 'checkUpdate', [
+//            args
+//        ]);
+//
+//    return promise;
+//};
 
 exports.checkUpdate = function (args, onfulfill, onreject) {
-    var ThenFail = window.ThenFail;
-    var promise;
-
-    if (ThenFail && !onfulfill && !onreject) {
-        promise = new ThenFail();
-    }
     if(!args) {
         args = {};
     }
-
-    cordova
-        .exec(function () {
-            if (promise) {
-                promise.resolve();
-            } else if (onfulfill) {
-                onfulfill();
-            }
-        }, function (err) {
-            if (promise) {
-                promise.reject(err);
-            } else if (onreject) {
-                onreject(err);
-            }
-        }, 'JzCU', 'checkUpdate', [
-            args
-        ]);
-
+    cordova.exec(onfulfill, onreject, 'JzCU', 'checkUpdate', [args]);
     return promise;
 };
